769169100be9b3d8007eed68ea3a610956ac6731
luojiahai <ffreylaw@gmail.com>
2018-05-18 19:29:55 +1000

    update

--- dnn.py
+++ dnn.py
@@ -14,10 +14,11 @@ LABELS_DICT = {"Clap": 0, "Cry": 1, "Disappoint": 2, "Explode": 3, "FacePalm": 4
 LABELS = ["Clap", "Cry", "Disappoint", "Explode", "FacePalm", 
     "Hands", "Neutral", "Shrug", "Think", "Upside"]
 TRAIN_DATASET_PATH = "data/train_raw.txt"
+DEV_DATASET_PATH = "data/dev_raw.txt"
 TEST_DATASET_PATH = "data/test_raw.txt"
 OUTPUT_FILENAME = "test_raw_output.txt"
 
-def load_train_dataset(path):
+def load_dataset(path):
     data = {}
     data["sentence"] = []
     data["emoji"] = []
@@ -62,31 +63,39 @@ def predict_and_output(estimator):
                 LABELS[test_dict[key][1]], test_dict[key][0]))
 
 def get_predictions(estimator, input_fn):
-  return [x["class_ids"][0] for x in estimator.predict(input_fn=input_fn)]
+    return [x["class_ids"][0] for x in estimator.predict(input_fn=input_fn)]
 
-def train_and_evaluate_with_module(hub_module, train_input_fn, predict_train_input_fn, train_module=False):
-        embedded_text_feature_column = hub.text_embedding_column(
-            key="sentence", module_spec=hub_module, trainable=train_module)
+def train_and_evaluate_with_module(hub_module, 
+    train_input_fn, predict_train_input_fn, predict_dev_input_fn, 
+    train_module=False):
+    embedded_text_feature_column = hub.text_embedding_column(
+        key="sentence", module_spec=hub_module, trainable=train_module)
 
-        estimator = tf.estimator.DNNClassifier(
-            hidden_units=[500, 100],
-            feature_columns=[embedded_text_feature_column],
-            n_classes=N_CLASSES,
-            optimizer=tf.train.AdagradOptimizer(learning_rate=0.003))
+    estimator = tf.estimator.DNNClassifier(
+        hidden_units=[500, 100],
+        feature_columns=[embedded_text_feature_column],
+        n_classes=N_CLASSES,
+        optimizer=tf.train.AdagradOptimizer(learning_rate=0.003))
 
-        estimator.train(input_fn=train_input_fn, steps=1000)
+    estimator.train(input_fn=train_input_fn, steps=1000)
 
-        train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
-        training_set_accuracy = train_eval_result["accuracy"]
+    train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
+    dev_eval_result = estimator.evaluate(input_fn=predict_dev_input_fn)
+    training_set_accuracy = train_eval_result["accuracy"]
+    dev_set_accuracy = dev_eval_result["accuracy"]
 
-        return {"training accuracy": training_set_accuracy}
+    return {
+        "training accuracy": training_set_accuracy,
+        "development accuracy": dev_set_accuracy
+    }
 
 def main():
     # reduce logging output
     tf.logging.set_verbosity(tf.logging.ERROR)
 
-    # load train data
-    train_df = load_train_dataset(TRAIN_DATASET_PATH)
+    # load train and dev data
+    train_df = load_dataset(TRAIN_DATASET_PATH)
+    dev_df = load_dataset(DEV_DATASET_PATH)
     train_df.head()
 
     # training input on the whole training set with no limit on training epochs
@@ -114,13 +123,17 @@ def main():
     # evaluate
     predict_train_input_fn = tf.estimator.inputs.pandas_input_fn(
         train_df, train_df["emoji"], shuffle=False)
+    predict_dev_input_fn = tf.estimator.inputs.pandas_input_fn(
+        dev_df, dev_df["emoji"], shuffle=False)
     train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
+    dev_eval_result = estimator.evaluate(input_fn=predict_dev_input_fn)
     print("training set accuracy: {accuracy}".format(**train_eval_result))
+    print("development set accuracy: {accuracy}".format(**dev_eval_result))
 
-    # confusion matrix
+    # confusion matrix on dev input
     with tf.Graph().as_default():
-        cm = tf.confusion_matrix(train_df["emoji"], 
-                                get_predictions(estimator, predict_train_input_fn))
+        cm = tf.confusion_matrix(dev_df["emoji"], 
+                                get_predictions(estimator, predict_dev_input_fn))
         with tf.Session() as session:
             cm_out = session.run(cm)
     cm_out = cm_out.astype(float) / cm_out.sum(axis=1)[:, np.newaxis]
@@ -132,15 +145,19 @@ def main():
     # transfer learning analysis
     results = {}
     results["nnlm-en-dim128"] = train_and_evaluate_with_module(
-        "https://tfhub.dev/google/nnlm-en-dim128/1", train_input_fn, predict_train_input_fn)
+        "https://tfhub.dev/google/nnlm-en-dim128/1", 
+        train_input_fn, predict_train_input_fn, predict_dev_input_fn)
     results["nnlm-en-dim128-with-module-training"] = train_and_evaluate_with_module(
-        "https://tfhub.dev/google/nnlm-en-dim128/1", train_input_fn, predict_train_input_fn, True)
+        "https://tfhub.dev/google/nnlm-en-dim128/1", 
+        train_input_fn, predict_train_input_fn, predict_dev_input_fn, True)
     results["random-nnlm-en-dim128"] = train_and_evaluate_with_module(
-        "https://tfhub.dev/google/random-nnlm-en-dim128/1", train_input_fn, predict_train_input_fn)
+        "https://tfhub.dev/google/random-nnlm-en-dim128/1", 
+        train_input_fn, predict_train_input_fn, predict_dev_input_fn)
     results["random-nnlm-en-dim128-with-module-training"] = train_and_evaluate_with_module(
-        "https://tfhub.dev/google/random-nnlm-en-dim128/1", train_input_fn, predict_train_input_fn, True)
+        "https://tfhub.dev/google/random-nnlm-en-dim128/1", 
+        train_input_fn, predict_train_input_fn, predict_dev_input_fn, True)
     print(results)
  
-
+# main
 if __name__ == "__main__":
     main()
